d60ffa065a853ac32fccac00036ec68ae1889def
benlongcroft <blongcroft@gmail.com>
2021-12-06 10:39:24 +0000

    added plots

--- Assets/Scripts/PlotController.cs
+++ Assets/Scripts/PlotController.cs
@@ -4,48 +4,30 @@ using UnityEngine;
 
 public class PlotController : MonoBehaviour
 {
-    public Sprite sml;
+    public GameObject[] subPlotObjs = {};
 
-    public Sprite med;
-
-    public Sprite lrg;
-
-    public GameObject child;
-    
-    private SpriteRenderer spriteRenderer;
-
-    private float timer = 0;
-
-    private int currentState = 0;
+    private bool[] subPlotStatus = {false, false, false, false};
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = child.gameObject.GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = null;
     }
-    
 
+    public void growOne()
+    {
+        for (int i = 0; i <= 4; i = i + 1)
+        {
+            if (subPlotStatus[i] == false)
+            {
+                subPlotController g = subPlotObjs[i].GetComponent<subPlotController>();
+                g.FlipSeeded();
+                subPlotStatus[i] = true;
+                break;
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        timer = timer + Time.deltaTime;
-        if (timer > 5)
-        {
-            switch (currentState)
-            {
-                case 0:
-                    spriteRenderer.sprite = sml;
-                    break;
-                case 1:
-                    spriteRenderer.sprite = med;
-                    break;
-                case 2:
-                    spriteRenderer.sprite = lrg;
-                    break;
-            }
-
-            currentState = currentState + 1;
-            timer = 0;
-        }
+        
     }
 }
